3c38ada569cb2a52234c2ba00a3a2a40fbd5060f
Yohanes <yohanesdahlan@gmail.com>
2024-07-20 10:07:53 +0700

    update tugas notepad

---
+++ Jawaban.sql
@@ -0,0 +1,33 @@
+Soal Laravel 1
+
+Yohanes Hubert Claudio
+
+1. Apa yang dimaksud dengan Laravel?
+b) Sebuah kerangka kerja (framework) PHP 
+
+2. Siapakah pencipta Laravel?
+a) Taylor Otwell X
+
+3. Manakah dari pernyataan berikut yang benar tentang Laravel?
+b) Laravel memudahkan pengembangan aplikasi web dengan PHP X
+
+4. Apa tujuan utama dari Laravel?
+a) Mempermudah pengembangan aplikasi web dengan PHP X
+
+5. Fungsi apa yang tidak termasuk dalam fitur-fitur Laravel?
+d) Graphics Design X
+
+6. Apa yang dimaksud dengan "Eloquent" dalam konteks Laravel?
+c) Sebuah fitur yang memudahkan interaksi dengan database menggunakan model x
+
+7. Fitur apa yang memungkinkan Anda mengelola dependencies dalam proyek Laravel?
+a) Composer X
+
+8. Bagaimana cara menginstal Laravel?
+b) Menggunakan perintah `composer create-project laravel/laravel nama-proyek` X
+
+9. Apa yang dimaksud dengan "Blade" dalam Laravel?
+b) Sebuah template engine bawaan Laravel X
+
+10. Apa yang dimaksud dengan "Middleware" dalam konteks Laravel?
+b) Sebuah lapisan yang ditempatkan di antara permintaan masuk dan respons keluar X
